a6af1b7189915cd460c9abdd465f21e419bca93e
Tatsuya Yatagawa <tatsy.mail@gmail.com>
2022-11-30 19:37:36 +0900

    Change Python test runner to pytest.

--- tests/python/test_filters.py
+++ tests/python/test_filters.py
@@ -1,7 +1,6 @@
 import os
-import unittest
 
-from nose2.tools import params
+import pytest
 
 import tinymesh as tm
 
@@ -16,69 +15,85 @@ filenames = [
 ]
 
 
-class TestFilters(unittest.TestCase):
-    @params(*filenames)
-    def test_smooth_laplacian(self, filename):
-        filename = os.path.join(CWD, model_dir, filename)
-        mesh = tm.Mesh(filename)
-        mesh.fill_holes()
+@pytest.mark.parametrize("filename", filenames)
+def test_smooth_laplacian(filename):
+    filename = os.path.join(CWD, model_dir, filename)
+    mesh = tm.Mesh(filename)
+    mesh.fill_holes()
 
-        try:
-            tm.smooth_laplacian(mesh)
-        except Exception:
-            self.fail('Failed!')
+    try:
+        tm.smooth_laplacian(mesh)
+    except Exception:
+        pytest.fail('Failed!')
 
-    @params(*filenames)
-    def test_smooth_taubin(self, filename):
-        filename = os.path.join(CWD, model_dir, filename)
-        mesh = tm.Mesh(filename)
-        mesh.fill_holes()
+    assert mesh.verify(), "Mesh verification failed!"
 
-        try:
-            tm.smooth_taubin(mesh)
-        except Exception:
-            self.fail('Failed!')
 
-    @params(*filenames)
-    def test_implicit_fairing(self, filename):
-        filename = os.path.join(CWD, model_dir, filename)
-        mesh = tm.Mesh(filename)
-        mesh.fill_holes()
+@pytest.mark.parametrize("filename", filenames)
+def test_smooth_taubin(filename):
+    filename = os.path.join(CWD, model_dir, filename)
+    mesh = tm.Mesh(filename)
+    mesh.fill_holes()
 
-        try:
-            tm.implicit_fairing(mesh)
-        except Exception:
-            self.fail('Failed!')
+    try:
+        tm.smooth_taubin(mesh)
+    except Exception:
+        pytest.fail('Failed!')
 
-    @params(*filenames)
-    def test_denoise_normal_gaussian(self, filename):
-        filename = os.path.join(CWD, model_dir, filename)
-        mesh = tm.Mesh(filename)
-        mesh.fill_holes()
+    assert mesh.verify(), "Mesh verification failed!"
 
-        try:
-            tm.denoise_normal_gaussian(mesh)
-        except Exception:
-            self.fail('Failed!')
 
-    @params(*filenames)
-    def test_denoise_normal_bilateral(self, filename):
-        filename = os.path.join(CWD, model_dir, filename)
-        mesh = tm.Mesh(filename)
-        mesh.fill_holes()
+@pytest.mark.parametrize("filename", filenames)
+def test_implicit_fairing(filename):
+    filename = os.path.join(CWD, model_dir, filename)
+    mesh = tm.Mesh(filename)
+    mesh.fill_holes()
 
-        try:
-            tm.denoise_normal_bilateral(mesh)
-        except Exception:
-            self.fail('Failed!')
+    try:
+        tm.implicit_fairing(mesh)
+    except Exception:
+        pytest.fail('Failed!')
 
-    @params(*filenames)
-    def test_denoise_l0_smooth(self, filename):
-        filename = os.path.join(CWD, model_dir, filename)
-        mesh = tm.Mesh(filename)
-        mesh.fill_holes()
+    assert mesh.verify(), "Mesh verification failed!"
 
-        try:
-            tm.denoise_l0_smooth(mesh)
-        except Exception:
-            self.fail('Failed!')
+
+@pytest.mark.parametrize("filename", filenames)
+def test_denoise_normal_gaussian(filename):
+    filename = os.path.join(CWD, model_dir, filename)
+    mesh = tm.Mesh(filename)
+    mesh.fill_holes()
+
+    try:
+        tm.denoise_normal_gaussian(mesh)
+    except Exception:
+        pytest.fail('Failed!')
+
+    assert mesh.verify(), "Mesh verification failed!"
+
+
+@pytest.mark.parametrize("filename", filenames)
+def test_denoise_normal_bilateral(filename):
+    filename = os.path.join(CWD, model_dir, filename)
+    mesh = tm.Mesh(filename)
+    mesh.fill_holes()
+
+    try:
+        tm.denoise_normal_bilateral(mesh)
+    except Exception:
+        pytest.fail('Failed!')
+
+    assert mesh.verify(), "Mesh verification failed!"
+
+
+@pytest.mark.parametrize("filename", filenames)
+def test_denoise_l0_smooth(filename):
+    filename = os.path.join(CWD, model_dir, filename)
+    mesh = tm.Mesh(filename)
+    mesh.fill_holes()
+
+    try:
+        tm.denoise_l0_smooth(mesh)
+    except Exception:
+        pytest.fail('Failed!')
+
+    assert mesh.verify(), "Mesh verification failed!"
