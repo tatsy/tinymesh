38e42a21c24b9e52cb142e7f76426ac68880762c
Tatsuya Yatagawa <tatsy.mail@gmail.com>
2023-11-30 14:45:22 +0900

    Update build script.

--- build_exts.py
+++ build_exts.py
@@ -1,56 +1,83 @@
 import re
-import pathlib
+import glob
 import platform
 
 from pybind11.setup_helpers import Pybind11Extension
 from setuptools.command.build_ext import build_ext
 
-exclude = ['src/tinymesh/ext']
-sources = pathlib.Path().glob('src/**/*.cpp')
-sources = [str(path).replace('\\', '/') for path in sources]
+
+class TinyMeshBuildExt(build_ext):
+    def run(self):
+        try:
+            build_ext.run(self)
+        except FileNotFoundError:
+            raise Exception("File not found. Could not compile C extension")
+
+    def build_extension(self, ext):
+        for e in self.extensions:
+            e.define_macros.extend(
+                [
+                    ("TINYMESH_PYTHON_MODULE", 1),
+                ]
+            )
+
+        if platform.system() == "Darwin":
+            for e in self.extensions:
+                e.extra_compile_args.extend(
+                    [
+                        "-mmacosx-version-min=10.15",
+                    ]
+                )
+
+        if self.compiler.compiler_type == "unix":
+            for e in self.extensions:
+                e.extra_compile_args.extend(
+                    [
+                        "-std=c++17",
+                    ]
+                )
+                e.extra_link_args.extend(
+                    [
+                        "-lstdc++fs",
+                    ]
+                )
+        elif self.compiler.compiler_type == "msvc":
+            for e in self.extensions:
+                e.define_macros.extend(
+                    [
+                        ("_CRT_SECURE_NO_WARNINGS", 1),
+                        ("_SILENCE_EXPERIMENTAL_FILESYSTEM_DEPRECATION_WARNING", 1),
+                    ]
+                )
+
+
+exclude = ["src/tinymesh/ext"]
+sources = glob.glob("src/**/*.cpp")
+sources = [str(path).replace("\\", "/") for path in sources]
 sources = [path for path in sources if all([not re.search(e, path) for e in exclude])]
 
-include_dirs = [
-    'src/tinymesh',
-    'src/tinymesh/ext/tinyobjloader',
-    'src/tinymesh/ext/tinyply/source',
-    'src/tinymesh/ext/eigen',
-    'src/tinymesh/ext/spectra/include',
+include = [
+    "src/tinymesh",
+    "src/tinymesh/ext/tinyobjloader",
+    "src/tinymesh/ext/tinyply/source",
+    "src/tinymesh/ext/eigen",
+    "src/tinymesh/ext/spectra/include",
 ]
 
-extra_compile_args = []
-extra_link_args = []
-define_macros = [('TINYMESH_PYTHON_MODULE', 1)]
-if platform.system() == "Windows":
-    define_macros.append(('_SILENCE_EXPERIMENTAL_FILESYSTEM_DEPRECATION_WARNING', 1))
-elif platform.system() == "Darwin":
-    extra_compile_args.extend([
-        '-std=c++17',
-        '-mmacosx-version-min=10.15',
-    ])
-else:
-    extra_compile_args.extend([
-        '-std=c++17',
-    ])
-    extra_link_args.extend([
-        '-lstdc++fs',
-    ])
-
 ext_modules = [
-    Pybind11Extension('tinymesh',
-                      sources,
-                      language='c++',
-                      include_dirs=include_dirs,
-                      extra_compile_args=extra_compile_args,
-                      extra_link_args=extra_link_args,
-                      define_macros=define_macros)
+    Pybind11Extension(
+        "tinymesh",
+        sources,
+        language="c++",
+        include_dirs=include,
+    )
 ]
 
 
 def build(setup_kwargs):
-    setup_kwargs.update({
-        'ext_modules': ext_modules,
-        'cmdclass': {
-            'build_ext': build_ext
-        },
-    })
+    setup_kwargs.update(
+        {
+            "ext_modules": ext_modules,
+            "cmdclass": {"build_ext": TinyMeshBuildExt},
+        }
+    )
